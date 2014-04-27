8c4fcfaaa0d4e61e124bc4647e64ac0c4e90a231
Arvin Singla <arvin.singla@gmail.com>
2014-04-27 15:56:58 -0400

    Initial configuration changes.

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,17 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'The REST of D8',
+    subtitle: '',
+    eventInfo: {
+      title: 'DrupalCamp Toronto 2014',
+      date: '05/02/2014'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'favicon.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,19 +21,15 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+    name: 'Arvin Singla',
+    company: 'Web Developer, Myplanet',
+    twitter: '@arvinsingla',
+    github: 'http://github.com/arvinsingla'
+  }, {
+    name: 'Sebastian Lesch',
+    company: 'Program Developer, Myplanet',
+    twitter: '@sebsebseb123',
+    github: 'http://github.com/sebsebseb123<br/>'
+  }]
 };
 
